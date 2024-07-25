556c719a3fc7cb11b9c684bcd4f6f04af78f9a1b
Lewis <hamiltonjlewis@gmail.com>
2024-07-24 21:48:35 -0500

    deployed with firebase

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="overflow-hidden">
-      <h1>Hello World</h1>
+      <h1>Hello World testing firebase!</h1>
       <Link href="/rsvp">RSVP Here!</Link>
     </main>
   );
